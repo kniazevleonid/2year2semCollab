f2cb82912936570ce545b4c9cf9801921e0591c5
KirillKrivosheev <36293477+KirillKrivosheev@users.noreply.github.com>
2018-02-15 23:03:58 +0300

    Update list.c

--- list.c
+++ list.c
@@ -1,1 +1,24 @@
-#include
+#include "mylist.h"
+
+void list_insert(pbox p, data d)
+{
+        pmylist l = (pmylist)p;
+        pnode tmp = (pnode)malloc(sizeof(node));
+        tmp->data = d;
+        if(l->head == NULL)
+        {
+                l->head = tmp;
+                tmp->next = tmp;
+                tmp->prev = tmp;
+        }
+        else
+        {
+                tmp->next = head;
+                tmp->prev = (l->head)->prev;
+                ((l->head)->prev)->next = tmp;
+                (l->head)->prev = tmp;
+        }
+        return;
+}
+
+
