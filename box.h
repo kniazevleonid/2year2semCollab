caedec39a2cf3d2e0312023f3955e23ea4d844e7
Knyazev Leonid <36293330+knyazevleonid@users.noreply.github.com>
2018-02-16 11:44:08 +0300

    Update box.h

--- box.h
+++ box.h
@@ -1,7 +1,9 @@
 typedef void* data;
 #define BOX_DECL \
-void(*insert)(struct box p, data d); \
-void(*mydelete)(struct box p);
+void(*insert)(struct box * p, data d); \
+void(*mydelete)(struct box * p); \
+void*(*search)(struct box * p, data d, int(*f)(void *, void *)); \
+void(*delete_element)(struct box * p, data d, int(*f)(void *, void *));
 
 struct box
 {
