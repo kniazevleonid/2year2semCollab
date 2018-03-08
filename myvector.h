aae30f0f1c9a626d03a5a95ccec1dc1d755a6acc
Knyazev Leonid <36293330+knyazevleonid@users.noreply.github.com>
2018-03-08 19:38:32 +0300

    Add files via upload

---
+++ myvector.h
@@ -0,0 +1,36 @@
+#include "box.h"
+
+struct node
+{
+    data data;
+	int num;
+} typedef node, * pnode;
+
+struct myvector
+{
+	BOX_DECL;
+	
+	pnode head;
+	pnode end;
+	int size;
+}typedef myvector, * pmyvector;
+
+void vector_insert(pbox p, data d);
+
+pbox vector_create();
+
+void vector_mydelete(pbox p);
+
+pentry vector_search (pbox p, data d, int(*f)(void *, void *));
+
+void vector_delete_element (pbox p, data d, int(*f)(void *, void *));
+
+pentry vector_first(pbox p);
+
+pentry vector_end(pbox p);
+
+pentry vector_get_next(pbox p, pentry elem);
+
+pentry vector_get_prev(pbox p, pentry elem);
+
+pentry vector_get_by_num(pbox p, int num);
