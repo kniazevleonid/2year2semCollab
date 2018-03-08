0eecd39580bb5ad6cf7a89559199ef61eca40c45
KirillKrivosheev <36293477+KirillKrivosheev@users.noreply.github.com>
2018-03-08 21:55:36 +0300

    Update box.h

--- box.h
+++ box.h
@@ -3,7 +3,7 @@ typedef void* data;
 void(*insert)(struct box * p, data d); \
 void(*mydelete)(struct box * p); \
 pentry(*search)(struct box * p, data d, int(*f)(void *, void *)); \
-void(*delete_element)(struct box * p, data d, int(*f)(void *, void *));\
+void(*delete_element)(struct box * p, pentry d);\
 pentry(*first)(struct box * p);\
 pentry(*last)(struct box * p);\
 pentry(*get_next)(struct box* p, pentry elem);\
