4f17969891981f8b8c8ca81ef3a5095a19c272d7
KirillKrivosheev <36293477+KirillKrivosheev@users.noreply.github.com>
2018-03-08 21:42:11 +0300

    Update box.h

--- box.h
+++ box.h
@@ -5,7 +5,7 @@ void(*mydelete)(struct box * p); \
 pentry(*search)(struct box * p, data d, int(*f)(void *, void *)); \
 void(*delete_element)(struct box * p, data d, int(*f)(void *, void *));\
 pentry(*first)(struct box * p);\
-pentry(*end)(struct box * p);\
+pentry(*last)(struct box * p);\
 pentry(*get_next)(struct box* p, pentry elem);\
 pentry(*get_prev)(struct box* p, pentry elem);\
 pentry(*get_by_num)(struct box* p, int i);
