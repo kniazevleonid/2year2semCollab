aa08edb7cc0efeaad2aa3eb1959c7cbfd5b7c580
KirillKrivosheev <36293477+KirillKrivosheev@users.noreply.github.com>
2018-04-13 11:57:28 +0300

    Update bubble_sort_for_dictionary.c

--- bubble_sort_for_dictionary.c
+++ bubble_sort_for_dictionary.c
@@ -3,15 +3,6 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-int MoreInt(void * x, void * y)
-{
-	int * a = (int *)x;
-	int * b = (int *)y;
-	if (* a > * b)
-		return 1;
-	return 0;
-}
-
 void sort(pbox p, int(*f)(void *, void *))
 {
         pentry cnt;
@@ -27,42 +18,3 @@ void sort(pbox p, int(*f)(void *, void *))
 	}
 	return;	
 }
-
-/*int main()
-{
-
-	pbox a = CREATE();
-
-	int mass[4];
-	mass[0] = 2;
-	mass[1] = 1;
-	mass[2] = 3;
-	mass[3] = 1;
-
-	pentry temp;
-
-	data d = &mass[0];
-
-	a->insert(a, d);
-
-	a->insert(a, d+4);
-
-	a->insert(a, d+8);
-
-	a->insert(a, d + 12);
-
-	for (temp = a->first(a); temp!=a->last(a); temp = a->get_next(a, temp))
-		printf ("%d\n", *(int*)temp->data);
-	printf ("%d\n\n", *(int*)temp->data);
-
-	sort(a, MoreInt);
-
-	for (temp = a->first(a); temp!=a->last(a); temp = a->get_next(a, temp))
-                printf ("%d\n", *(int*)temp->data);
-	printf ("%d\n\n", *(int*)temp->data);
-
-	a->mydelete(a);
-	
-	return 0;
-}
-*/
