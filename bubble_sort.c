e76a32beb0dbe70591a6089095ba8adb396f71f8
KirillKrivosheev <36293477+KirillKrivosheev@users.noreply.github.com>
2018-04-13 11:58:13 +0300

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -12,22 +12,6 @@ int MoreInt(void * x, void * y)
 	return 0;
 }
 
-void sort(pbox p, int(*f)(void *, void *))
-{
-        pentry cnt;
-	pentry finish;
-
-	for (finish = p->last(p); finish != p->first(p); finish = p->get_prev(p,finish))
-	{
-		for (cnt = p->first(p); cnt != finish; cnt = p->get_next(p, cnt))
-		{
-			if (f(cnt->data, (p->get_next(p, cnt))->data))
-				p->myswitch(p, cnt, p->get_next(p, cnt));
-		}
-	}
-	return;	
-}
-
 int main()
 {
 
